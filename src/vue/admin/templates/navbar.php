81c75d0e3980eaa9eb03acf5571ea342085db68e
Jérôme Hillion <jerome.hillion@protonmail.com>
2022-01-26 12:44:45 +0100

    mise en place css

---
+++ src/vue/admin/templates/navbar.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../../../../public/css/admin/templates/navbar.css">
+    <link rel="stylesheet" href="../../../../public/css/normalize.css">
+    <title>navbar</title>
+</head>
+<body>
+<div class="navbar">
+    <div class="containerLeft">
+        <div class="menu">
+            <img src="../../../../public/css/admin/icones/interface-de-tableau-de-bord.png" alt="icone dashboard"><h3>Tableau de bord</h3>
+        </div>
+    </div>
+    <div class="containerMiddle">
+        <div class="subMenu">
+            <a href=""><img src="../../../../public/css/admin/icones/ajouter-un-bouton.png" alt="bouton ajouter"> <h3> Ajouter un livre</h3></a>
+        </div>
+        <div class="subMenuTitle">
+            <h2>Librairie Digital</h2>
+        </div>
+    </div>
+  <div class="containerRight">
+      <div class="subMenuParams">
+          <a href="#">Page d'accueil</a>
+      </div>
+  </div>
+
+</div>
+</body>
+</html>
