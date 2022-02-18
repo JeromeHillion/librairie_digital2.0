9a520638c5f3a578893545aa14d072b99650a11e
Jérôme Hillion <jerome.hillion@protonmail.com>
2022-02-18 12:11:34 +0100

    mise en place du style sur  les pages menu2, book, details books, dashboard

--- src/vue/admin/book/detailsBook.php
+++ src/vue/admin/book/detailsBook.php
@@ -16,58 +16,73 @@
     <?php
     include_once '../../../vue/admin/templates/menu2.php';
     ?>
-    <?php if ($book) :?>
+    <?php if ($book) : ?>
 
     <div class="container">
-        <?php if ($book['data']['language'] === "Français") :?>
-            <h3><?= $book['data']['title']?></h3>
-            <p><?= $book['data']['categories']?></p>
-        <p>De:
-            <?php foreach ($book['data']['authors'] as $author):?>
-               <?=$author ?>
-            <?php endforeach;?>
-            </p>
-        <p><?= $book['data']['publishedDate']?></p>
-        <img src="<?= $book['data']['imageLinks']?>" alt="thumbnail">
-        <p>Résumé</p>
-        <p> <?= $book['data']['description']?></p>
-        <?php endif; ?>
+        <?php if ($book['data']['language'] === "Français") : ?>
+        <div class="info">
+            <div class="details">
+                <p><?= "<span>Titre du livre : </span> " . $book['data']['title'] ?></p>
+                <p><?= "<span>Catégorie : </span>" . $book['data']['categories'] ?></p>
+                <p>
+                    <?php foreach ($book['data']['authors'] as $author): ?>
+                        <?= "<span>Auteur(e) : </span>" . $author ?>
+                    <?php endforeach; ?>
+                </p>
+                <p><?= "<span> Date de publication : </span> " . $book['data']['publishedDate'] ?></p>
+            </div>
+            <div class="picture">
+                <img src="<?= $book['data']['imageLinks'] ?>" alt="thumbnail">
+            </div>
+        </div>
+        <div class="description">
+            <h3>Résumé</h3>
+            <p> <?= $book['data']['description'] ?></p>
+            <?php endif; ?>
 
-        <?php if ($book['data']['language'] === "English") :?>
-            <h3><?= $book['data']['title']?></h3>
-            <p><?= $book['data']['categories']?></p>
-        <?php foreach ($book['data']['authors'] as $author):?>
-        <p>By: <?=$author ?></p>
-        <?php endforeach;?>
-        <p><?= $book['data']['publishedDate']?></p>
-        <img src="<?= $book['data']['imageLinks']?>" alt="thumbnail">
+            <?php if ($book['data']['language'] === "Français") : ?>
+                <h4>Description du produit</h4>
+                <p><span>ISBN : </span> <?= $book['data']['isbn'] ?></p>
+                <p><span>Nombre de pages : </span> <?= $book['data']['pageCount'] ?></p>
+                <p><span>Langue : </span> <?= $book['data']['language'] ?></p>
+                <p><span>Catégorie:</span> <?= $book['data']['categories'] ?></p>
+            <?php endif; ?>
+            <form action="crud/addBookFormController.php" method="POST">
+                <input type="hidden" name="isbn" value="<?= $book['data']['isbn'] ?>">
+                <input class="btnAdd" type="submit" value="Ajouter">
+            </form>
+        </div>
+        <?php if ($book['data']['language'] === "English") : ?>
+        <div class="info"><div class="details">
+        <p><?="<span> Title : </span>". $book['data']['title'] ?></p>
+            <p><?="<span>Category : </span>". $book['data']['categories'] ?></p>
+            <?php foreach ($book['data']['authors'] as $author): ?>
+                <p><span>By: </span><?= $author ?></p>
+            <?php endforeach; ?>
+            <p><?= $book['data']['publishedDate'] ?></p>
+        </div>
+            <div class="picture">
+            <img src="<?= $book['data']['imageLinks'] ?>" alt="thumbnail">
+        </div>
+        </div>
             <h2>Summary</h2>
-        <p><?= $book['data']['description']?></p>
+        <div class="description">
+            <p><?= $book['data']['description'] ?></p>
         <?php endif; ?>
-        <hr>
-
-        <?php if ($book['data']['language'] === "Français") :?>
-            <h3>Description du produit</h3>
-            <p>ISBN: <?= $book['data']['isbn']?></p>
-            <p>Nombre de pages: <?= $book['data']['pageCount']?></p>
-            <p>Langue: <?= $book['data']['language']?></p>
-            <p>Catégorie: <?= $book['data']['categories']?></p>
-        <?php endif; ?>
-        <?php if ($book['data']['language'] === "English") :?>
+        <?php if ($book['data']['language'] === "English") : ?>
             <h2>Product Description</h2>
-            <p>ISBN: <?= $book['data']['isbn']?></p>
-            <p>Number of pages: <?= $book['data']['pageCount']?></p>
-            <p>Language: <?= $book['data']['language']?></p>
-            <p>Category: <?= $book['data']['categories']?></p>
+            <p><span>ISBN : </span><?= $book['data']['isbn'] ?></p>
+            <p><span>Number of pages : </span><?= $book['data']['pageCount'] ?></p>
+            <p><span>Language :</span> <?= $book['data']['language'] ?></p>
+            <p><span>Category : </span><?= $book['data']['categories'] ?></p>
+            <form action="crud/addBookFormController.php" method="POST">
+                <input type="hidden" name="isbn" value="<?= $book['data']['isbn'] ?>">
+                <input class="btnAdd" type="submit" value="Ajouter">
+            </form>
         <?php endif; ?>
+<?php endif; ?>
+        </div>
 
-
-        <form action="crud/addBookFormController.php" method="POST">
-            <input type="hidden" name="isbn" value="<?= $book['data']['isbn'] ?>">
-            <input class="btnAdd" type="submit" value="Ajouter">
-        </form>
-
-    <?php endif;?>
     </div>
 </div>
 <script src="../../../../public/js/admin/templates/navbar.js"></script>
